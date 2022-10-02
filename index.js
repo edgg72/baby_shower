30efdaa46ea5fdefe1993d88ce073561f75e68d8
edgg72 <edgg72@hotmail.compwd>
2022-10-02 14:36:09 -0500

    fixinng the button bug

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const openBtn = document.querySelector(".js-card-opener");
 const buttons = document.querySelectorAll(".botones a");
+const buttonsContainer = document.querySelector(".botones a");
+const comoLLegar = document.getElementById("btn1")
+const confirmar = document.getElementById("btn2")
 
 openBtn.onclick = function () {
+  if(buttonsContainer.classList.contains("show")){
+    comoLLegar.href = "https://goo.gl/maps/NjH6tPajvkPgnAt37";
+    confirmar.href = "https://api.whatsapp.com/send?phone=+573235903858&text=Gracias%20por%20la%20invitación%20al%20baby%20shower,%20con%20gusto%20asistiremos!%20🥳";
+  } else {
+    comoLLegar.removeAttribute("href");
+    confirmar.removeAttribute("href");
+
+  }
   document.body.classList.toggle("open");
   buttons.forEach(button => {
     button.classList.toggle("show")
   })
+  console.log( buttonsContainer.classList.contains("show"))
 };
